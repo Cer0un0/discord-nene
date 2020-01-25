977800a9bcb65aad57a4cc8be0a606ddb6a4eb76
Cer0un0 <wataru.mrt@gmail.com>
2020-01-26 01:09:55 +0900

    nene ikikaere

--- discordbot.py
+++ discordbot.py
@@ -6,8 +6,7 @@ import random as ra
 import os
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'NjY2NTg2ODY1OTMwODYyNjA0.XixlXg.hUdDg_G2cnSbsspzf2WHft8f_vA'
-#TOKEN = os.environ["TOKEN"]
+TOKEN = os.environ["TOKEN"]
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
