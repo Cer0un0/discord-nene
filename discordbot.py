eac95b6e74100f7ec3ee5031dfed84b30661e401
Cer0un0 <wataru.mrt@gmail.com>
2020-03-23 01:52:49 +0900

    kill /tenki

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,6 @@
 # インストールした discord.py を読み込む
 import discord
 import sys
-import requests
 import random as ra
 import os
 sys.dont_write_bytecode = True
@@ -27,57 +26,6 @@ async def on_ready():
     await channel.send("墨です!!!おはようございます!!!!!")
 # メッセージ受信時に動作する処理
 
-
-async def weather(S):
-    print('unko')
-    url = build_url(S)
-    json_data = request_json(url)
-    print(json_data)
-    in_one_hour_rainfall = extract_rainfall(json_data)
-    print(json_data)
-    return json_data
-
-
-    if in_one_hour_rainfall >= 10:
-        rainfall_notice = notification_level(in_one_hour_rainfall)
-
-
-# url組み立て
-def build_url(S):
-    APP_ID = "dj00aiZpPU4wM2tLc1hIREZWWiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDU-"
-    BASE_URL = "http://weather.olp.yahooapis.jp/v1/place"
-    if S[1] == 'Osaka' or S[1] == 'O':
-        COORDINATES = '34.691576, 135.506632'
-    elif S[1] == 'Hakata' or S[1] == 'H':
-        COORDINATES = '33.589412, 130.421062'
-    elif S[1] == 'Machida' or S[1] == 'M':
-        COORDINATES = '35.541994, 139.445376'
-    elif S[1] == 'Kawasaki' or S[1] == 'K':
-        COORDINATES = '35.530833, 139.702912'
-    else: return 'Osaka(O), Hakata(H), Machida(M), Kawasaki(K)から選んでね'
-    OUTPUT = "json"
-
-    url = BASE_URL + "?appid=%s&coordinates=%s&output=%s" % (APP_ID, COORDINATES, OUTPUT)
-
-    return url
-
-
-# リクエスト
-def request_json(url):
-    req = requests.get(url)
-    json_data = req.json()
-
-    return json_data
-
-
-# 降水強度の取得
-def extract_rainfall(json_data):
-    weather = json_data['Feature'][0]['Property']['WeatherList']['Weather']
-    in_one_hour_rainfall = weather[1]['Rainfall']
-    print(json_data)
-    return json_data
-
-    return in_one_hour_rainfall
 
 def ra_file_line(input_file):
     f = open(input_file, 'r')
@@ -136,9 +84,6 @@ def sugge(message, split_s):
         return 'さとりのちんぽのがでっけーよ' + '！'*ex_num
 
     return '/washlet'
-
-
-
 
 
 @client.event
